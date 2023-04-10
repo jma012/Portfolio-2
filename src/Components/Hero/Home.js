92735762deec5ea82133829fa08f0a75c94521aa
JM-creater <garadojosephmartin98@gmail.com>
2023-04-10 14:29:22 +0800

    initial commit

---
+++ src/Components/Hero/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Home.css"
+import hero from "../pic/hero.png"
+
+
+const Home = () => {
+  return (
+    <>
+        <section className='hero' id='home'>
+            <div className='container f_flex top'>
+                <div className='left top'>
+                    <h3>WELCOME TO </h3>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Home
